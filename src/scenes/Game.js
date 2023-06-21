9f68eec3653e6d762b5bdf32386ca1c103fbddf5
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-06-21 13:54:34 +0200

    added Platforms

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,14 +6,17 @@ export default class Game extends Phaser.Scene
   {
     super('game')
   }
-
+  
   preload()
   {
     this.load.image('background', 'assets/bg_layer1.png')    
+    this.load.image('platform', 'assets/ground_grass.png')
   }
-
+  
   create()
   {
     this.add.image(240,320, 'background')
+    this.physics.add.image(240,320, 'platform')
+    .setScale(0.5)
   }
 }
