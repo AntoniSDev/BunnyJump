1fbac4afdc929c3a690775cee4f9f78f60afdf01
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-06-21 15:46:41 +0200

    Auto jump

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,6 +2,9 @@ import Phaser from '../lib/phaser.js'
 
 export default class Game extends Phaser.Scene
 {
+  
+  /** @type {Phaser.Physics.Arcade.Sprite} */
+  player
   constructor()
   {
     super('game')
@@ -11,7 +14,7 @@ export default class Game extends Phaser.Scene
   {
     this.load.image('background', 'assets/bg_layer1.png')    
     this.load.image('platform', 'assets/ground_grass.png')
-
+    
     this.load.image('bunny-stand', 'assets/bunny1_stand.png')
   }
   
@@ -23,20 +26,34 @@ export default class Game extends Phaser.Scene
     {
       const x = Phaser.Math.Between(80, 400)
       const y = 150 * i
-
+      
       /** @type {Phaser.Physics.Arcade.Sprite} */
       const platform = platforms.create(x, y, 'platform')
-      platform.scale = 0.5
-
+      platform.scale = 0.4
+      
       /** @type {Phaser.Physics.Arcade.StaticBody} */
       const body = platform.body
       body.updateFromGameObject()
-
-      const player = this.physics.add.sprite(240, 320, 'bunny-stand')
+    }
+      
+      this.player = this.physics.add.sprite(240, 320, 'bunny-stand')
       .setScale(0.5)
       
-      this.physics.add.collider(platforms, player)
+      this.physics.add.collider(platforms, this.player)    
       
+      this.player.body.checkCollision.up = false
+      this.player.body.checkCollision.left = false
+      this.player.body.checkCollision.right = false
+  }
+  
+  
+  update()
+  {     
+    const touchingDown = this.player.body.touching.down
+    
+    if (touchingDown)
+    {
+      this.player.setVelocityY(-300)
     }
   }
 }
